c48f264174f96fbc9507b8aa957da842cba1ecfd
Angel Tobing <ngellllv@gmail.com>
2025-04-12 00:00:27 +0700

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {extend: {
+      fontFamily: {
+        sans: ['Poppins']
+      }
+    }
+  },
   },
   plugins: [],
 }
